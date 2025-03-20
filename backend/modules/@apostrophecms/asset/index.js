126a562d14dffedbadd7ac3b8e860c440f400599
Robert Means <robert@robertmeans.net>
2025-03-20 09:20:24 -0400

    Remove unintended file from PR

--- backend/modules/@apostrophecms/asset/index.js
+++ backend/modules/@apostrophecms/asset/index.js
@@ -2,6 +2,9 @@ export default {
   // When not in production, refresh the page on restart
   options: {
     refreshOnRestart: true,
+    breakpointPreviewMode: {
+      enabled: false
+    },
     hmr: false
   }
 };
